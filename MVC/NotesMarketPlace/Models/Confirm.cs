e0c58bb0b7107fd5786696c1124f2ab569cf3ac0
VJaimin29 <74224973+VJaimin29@users.noreply.github.com>
2021-03-08 18:34:49 +0530

    coomit1

---
+++ MVC/NotesMarketPlace/Models/Confirm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using NotesMarketPlace.Models;
+
+namespace NotesMarketPlace.Models
+{
+    public class Confirm
+    {
+        //User u = new User();
+
+        [Required(ErrorMessage = "Please enter First Name")]
+        public string First_Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter Last Name")]
+        public string Last_Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter Email ID")]
+        public string EmailID { get; set; }
+
+        [Required(ErrorMessage = "Please enter Password")]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public String ConfirmPassword { get; set; }
+    }
+}
---
+++ MVC/NotesMarketPlace/Models/addNotes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotesMarketPlace.Models
+{
+    public class addNotes
+    {
+        //Seller Notes
+        public int SellerID { get; set; }
+        public int Status { get; set; }
+        public Nullable<int> ActionedBy { get; set; }
+        public string AdminRemarks { get; set; }
+        public Nullable<System.DateTime> PublishedDate { get; set; }
+        public string Title { get; set; }
+        public int Category { get; set; }
+        public string DisplayPicture { get; set; }
+        public Nullable<int> NoteType { get; set; }
+        public Nullable<int> NumberOfPages { get; set; }
+        public string Description { get; set; }
+        public string UniversityName { get; set; }
+        public Nullable<int> Country { get; set; }
+        public string Course { get; set; }
+        public string CourseCode { get; set; }
+        public string Professor { get; set; }
+        public bool IsPaid { get; set; }
+        public decimal SellingPrice { get; set; }
+        public string NotesPreview { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<int> CreateedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public Nullable<int> ModifiedBy { get; set; }
+        public bool IsActive { get; set; }
+
+        //Seller Notes Attachments
+        public int NoteID { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+
+        public class NotesCategory<t1 ,t2>
+        {
+            public t1 ID { get; set; }
+            public t2 Name { get; set; }
+            public IEnumerable<SelectListItem> Item { get; set; }
+        }
+
+        public class NotesTypes<t1, t2>
+        {
+            public t1 ID { get; set; }
+            public t2 Name { get; set; }
+            public IEnumerable<SelectListItem> Item3 { get; set; }
+        }
+
+        public class Countries<t1 , t2>
+        {
+            public t1 ID { get; set; }
+            public t2 Name { get; set; }
+            public IEnumerable<SelectListItem> Item1 { get; set; }
+        }
+    }
+
+}
